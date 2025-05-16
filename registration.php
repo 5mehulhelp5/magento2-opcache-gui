95e7d9ecd9bed993faa269a88936e55e2db71a0c
Ilan Parmentier <2975845+iparmentier@users.noreply.github.com>
2025-05-16 20:33:22 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Copyright ©  All rights reserved.
- * See COPYING.txt for license details.
+ * OPcache GUI module registration
+ *
+ * @category  Genaker
+ * @package   Genaker_Opcache
+ * @author    Yehor Shytikov, Ilan Parmentier
+ * @copyright Copyright © 2020-2025 Genaker, Amadeco. All rights reserved.
+ * @license   MIT License
  */
+declare(strict_types=1);
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Genaker_Opcache', __DIR__);
-
